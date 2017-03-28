80f10272c474caabfd5aad838afc82f8051ddd9c
Brok <Ingrid.Brok@ordina.nl>
2017-03-28 08:40:18 +0200

    Teller toegevoegd, bij afloop opent highscores.html

--- www/js/SpelBeheerder.js
+++ www/js/SpelBeheerder.js
@@ -4,28 +4,33 @@ function SpelBeheerder(){
     this.knop;
     this.knopPositieDienst;
     this.score;
+    this.teller;
 
     // start functie, wordt uitgevoerd bij begin spel
     this.start = function(){
         this.knopFabriek = new KnopFabriek();
         this.knopPositieDienst = new KnopPositieDienst();
         this.score = new Score();
-
+        this.teller = new Teller();
         this.toonNieuweKnop();
+        this.score.toonScore();
+
     }
 
     this.toonNieuweKnop = function(){
         this.knop = this.knopFabriek.krijgWillekeurigeKnop();
-
         this.knop.klikLuisteraar = this.erIsOpEenKnopGeklikt;
         this.knopPositieDienst.toonKnop(this.knop);
     }
 
     this.erIsOpEenKnopGeklikt = function() {
       this.score.score += this.knop.waarde;
-
-      document.getElementById("Score").innerHTML = this.score.score;
-
+      this.score.toonScore();
       this.toonNieuweKnop();
+    }
+    
+    this.eindeSpel = function(){
+      window.open("../www/highscores.html","_self");
+      alert("De tijd is om! \nJou score: " + this.score.score + " punten!");
     }
 }
---
+++ www/js/Teller.js
@@ -0,0 +1,18 @@
+function Teller(){
+  document.getElementById("Teller").innerHTML = "0:00"
+  this.aftellen = function() {
+    var seconden = 30;
+    function tik() {
+        var afteller = document.getElementById("Teller");
+        seconden--;
+        afteller.innerHTML = "0:" + (seconden < 10 ? "0" : "") + String(seconden);
+        if( seconden > 0 ) {
+            setTimeout(tik, 1000);
+        } else {
+            spelBeheerder.eindeSpel();
+        }
+    }
+    tik();
+  }
+  this.aftellen();
+}
